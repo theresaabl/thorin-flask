af2ef71ed69f68324a017cbecffbc829f4f83622
Theresa Abl <theresaabl@gmx.at>
2024-12-07 12:34:52 +0000

    Create contact page and link all pages using jinja template method

--- run.py
+++ run.py
@@ -5,9 +5,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# routing
 @app.route("/")
-def index():
+def index(): # this is a view
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
